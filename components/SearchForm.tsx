2ccda610a97b8961f9e73e74e84f4f3f0b797095
ShivamMehta20 <shivamm9784942@gmail.com>
2024-12-28 20:14:19 +0530

    first commit

---
+++ components/SearchForm.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Form from "next/form";
+import {SearchFormReset} from "@/app/components/SearchFormReset";
+import {Search} from "lucide-react";
+
+export const SearchForm = ({query}:{query?:string}) => {
+    return (
+
+        <Form action='/' scroll={ false} className="search-form">
+            <input type="text"
+                   defaultValue={query}
+                   className="search-input"
+                   name="query"
+                   placeholder="Search"/>
+            <div className="flex gap-2">
+                {query && <SearchFormReset/>}
+                <button type="submit" className="search-btn text-white">
+                        <Search className="size-5"/>
+                </button>
+            </div>
+            </Form>
+    )
+}
